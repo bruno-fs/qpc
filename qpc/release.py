8bb1173a39a366f232756ae22d3a2688852c9c46
Bruno Ciconelle <bciconel@redhat.com>
2022-07-27 12:15:23 -0300

    Rebrand qpc as dsc

--- qpc/release.py
+++ qpc/release.py
@@ -2,6 +2,6 @@
 VERSION = "1.0.1"
 AUTHOR = "QPC Team"
 AUTHOR_EMAIL = "qpc@redhat.com"
-PKG_NAME = "qpc"
+PKG_NAME = "dsc"
 ENTRYPOINT = f"{PKG_NAME}=qpc.__main__:main"
 URL = "https://github.com/quipucords/qpc"
